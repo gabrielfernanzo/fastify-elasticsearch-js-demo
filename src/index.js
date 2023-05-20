8177d4c03d2567a3cc696c040a71afa6ad7e9cc6
gabrielFernandes-dev <gabriel.work076@gmail.com>
2023-05-20 14:30:42 -0300

    feat: add index & search routes

--- src/index.js
+++ src/index.js
@@ -16,6 +16,54 @@ fastify.get('/', (_request, reply) => {
   };
 });
 
+fastify.post(`/reminders`, async (request, reply) => {
+  try {
+    const { result } = await fastify.elastic.index({
+      index,
+      op_type: 'create',
+      document: { ...request.body, created_at: new Date() },
+    });
+
+    reply.statusCode = 201;
+    return { success: true, result };
+  } catch (excepiton) {
+    reply.statusCode = 500;
+
+    return { success: false, excepiton };
+  }
+});
+
+fastify.get(`/reminders/_search`, async (request, reply) => {
+  try {
+    const { page, size, ...query } = request.query;
+
+    const q = Object.entries(query)
+      .map((search) => {
+        return search.join(':');
+      })
+      .join(' and ');
+
+    console.log({ q });
+
+    const {
+      hits: { hits: result },
+    } = await fastify.elastic.search({
+      index,
+      search_after: page,
+      size,
+      q,
+    });
+
+    reply.statusCode = 200;
+
+    return { success: true, result };
+  } catch (excepiton) {
+    reply.statusCode = 500;
+
+    return { success: false, excepiton };
+  }
+});
+
 fastify.listen({ port, host: '0.0.0.0' }, (error) => {
   if (error) {
     console.error(error);
